0491a5636cdcafea7dd528d058f31e8c7d39c695
ShijuPJohn <shijupjohn4u@gmail.com>
2022-10-08 12:33:44 +0530

    feat: add student to db and display students on table in the index page

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_app():
         app.config.from_object(LocalDevelopmentConfig)
     db.init_app(app)
     app.app_context().push()
+    db.create_all()
     return app
 
 
--- application/controllers.py
+++ application/controllers.py
@@ -1,11 +1,31 @@
-from flask import render_template, current_app as app
+from flask import render_template, current_app as app, request, redirect
+
+from application.models import Student, db
 
 
 @app.route('/', methods=['GET'])
 def index_get():
-    return render_template("index.html")
+    students = []
+    students = Student.query.all()
+    print(students)
+    return render_template("index.html", students=students)
 
 
 @app.route('/articles', methods=['GET'])
 def articles_get():
     return render_template("articles.html")
+
+
+@app.route('/student/create', methods=['GET'])
+def create_students_get():
+    return render_template("create_student.html")
+
+
+@app.route('/student/create', methods=['POST'])
+def create_students_post():
+    print(request.form)
+    form = request.form
+    student = Student(roll_number=form["roll"], first_name=form["f_name"], last_name=form["l_name"])
+    db.session.add(student)
+    db.session.commit()
+    return redirect("/")
